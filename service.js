6c75b8978d57314cc308cb1da2ccb0165cd578bf
Piyush Pathak <piyush.pathak@heplchat.in>
2016-01-12 17:59:30 +0530

    added tracking with latest lat long

--- service.js
+++ service.js
@@ -1,28 +1,44 @@
 var db = require('./db');
+var uberBook = require('./uber_book');
 
 module.exports.getTrackInfo = function(callback) {
-    db.get_active_location(function(err, latLongInfo) {
-        if (err || (latLongInfo && latLongInfo.length)) {
-            var id = "";
-            var lat = "";
-            var long = "";
 
-            for (var i in latLongInfo) {
-                id = latLongInfo[i].id;
-                lat = latLongInfo[i].loc.split(",")[0];
-                long = latLongInfo[i].loc.split(",")[1];
+    uberBook.requestCurrent(function(error, currentResponse){
+        console.log("current response " + currentResponse);
+        var lat = "";
+        var long = "";
+        if(!error) {
+            if(currentResponse.status == 'in_progress') {
+                lat = currentResponse.destination.latitude;
+                long = currentResponse.destination.longitude;
             }
-            db.expire_location(id, function (err, response) {
-                var response = {};
-                if(!err) {
+            var response = {};
+            db.get_active_location(function(err, latLongInfo) {
+                if (!err && (latLongInfo && latLongInfo.length)) {
+                    var id = "";
+                    for (var i in latLongInfo) {
+                        id = latLongInfo[i].id;
+                        lat = latLongInfo[i].loc.split(",")[0];
+                        long = latLongInfo[i].loc.split(",")[1];
+                    }
+                    db.expire_location(id, function (err, response) {
+                        if(!err) {
+                            response.lat = lat;
+                            response.long = long;
+                        }
+                        callback(null, response);
+                    });
+                } else {
                     response.lat = lat;
                     response.long = long;
-
+                    callback(null, response);
                 }
-                callback(null, response);
             });
-        } else {
-            callback(true);
+
         }
     });
+
+
+
+
 }
